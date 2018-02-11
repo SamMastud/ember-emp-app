df4852ace16580235e8d3780366a8c464347936f
samadhan.mastud@niyuj.com <samadhan.mastud@niyuj.com>
2018-02-11 19:58:04 +0530

    Added gitignore file

--- testem.js
+++ testem.js
@@ -5,8 +5,28 @@ module.exports = {
   "launch_in_ci": [
     "PhantomJS"
   ],
+<<<<<<< HEAD
   "launch_in_dev": [
     "PhantomJS",
     "Chrome"
   ]
+=======
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        // --no-sandbox is needed when running Chrome inside a container
+        process.env.TRAVIS ? '--no-sandbox' : null,
+
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ].filter(Boolean)
+    }
+  }
+>>>>>>> 4e5f3fc... message
 };
