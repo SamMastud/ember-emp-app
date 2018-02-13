7c48cee5ab7c749c0a8879d8b702e915bf0077c8
samadhan.mastud@niyuj.com <samadhan.mastud@niyuj.com>
2018-02-13 15:46:47 +0530

    Save user in progress

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,11 +1,21 @@
 import Route from '@ember/routing/route';
+import { hash } from 'rsvp';
 
 export default Route.extend({
     model() {
-        return this.store.findAll('user');
-    },
+		return hash({
+            users: this.store.findAll('user'),
+            user: null
+		});
+	},
     actions: {
-        toggleModal(self) {
+        toggleModal(self, model) {
+            let newuser = this.store.createRecord('user')
+			model.user = newuser;
+            self.toggleProperty('isShowingModal');
+        },
+        saveUser(self, user) {
+            user.save()
             self.toggleProperty('isShowingModal');
         }
     }
